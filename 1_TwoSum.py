0d0ce9efcfdf7ce7a564dc27d28dcec05fff19e9
肖建雄 <jx0xiao@gmail.com>
2017-12-29 01:24:33 +0800

    完成第一题

---
+++ 1_TwoSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        dic = dict()
+        for index,value in enumerate(nums):
+            difference = target - value
+            if difference in dic:
+                return [dic[difference],index]
+            else:
+                dic[value] = index
