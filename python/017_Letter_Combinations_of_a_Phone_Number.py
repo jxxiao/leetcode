0cb804dfdd1bb01e46e347317920171a4c8d93d1
jxxiao <jx0xiao@gmail.com>
2018-11-26 15:09:59 +0800

    finish 017

---
+++ python/017_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def __init__(self):
+        self.num_value = {
+                '2': ['a', 'b', 'c'],
+                '3': ['d', 'e', 'f'],
+                '4': ['g', 'h', 'i'],
+                '5': ['j', 'k', 'l'],
+                '6': ['m', 'n', 'o'],
+                '7': ['p', 'q', 'r', 's'],
+                '8': ['t', 'u', 'v'],
+                '9': ['w', 'x', 'y', 'z']
+            }
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+
+        if digits == "":
+            return []
+        out = ['']
+        for i in range(len(digits)):
+            out = self.compostion(digits[i], out)
+        return out
+
+    def compostion(self, digit, out):
+        k = [j+i for i in self.num_value[digit] for j in out]
+        return k
